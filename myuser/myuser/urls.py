dfa80910b5dafb4a6eca55a27d7f1460fc978a6e
J-Huang <1009098722@qq.com>
2017-07-26 09:41:02 +0800

    add password_change and reset_password

--- myuser/myuser/urls.py
+++ myuser/myuser/urls.py
@@ -24,5 +24,14 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^register/$', core_views.register, name='register')
+    url(r'^register/$', core_views.register, name='register'),
+    url(r'^change/$', auth_views.password_change, {'template_name': 'change.html'}, name='change'),
+    url(r'^change_done/$', auth_views.password_change_done, {'template_name': 'change_done.html'}, name='password_change_done'),
+
+    url(r'^reset/$', auth_views.password_reset, {'template_name': 'reset.html'}, name='reset'),
+    url(r'^password_reset_done/$', auth_views.password_reset_done, {'template_name': 'password_reset_done.html'}, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,
+        {'template_name': 'reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'reset_complete.html'},
+        name='password_reset_complete'),
 ]
