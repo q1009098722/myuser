3e88f8c986eee074b69153d0d83b40055cbd3f1e
J-Huang <1009098722@qq.com>
2017-08-10 14:08:23 +0800

    test

--- myuser/users/tests.py
+++ myuser/users/tests.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
+client = Client()
 
 # Create your tests here.
+class Test_Register(TestCase):
+    def test_register(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "formc", status_code=200)
